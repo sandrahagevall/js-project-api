304cf34911d44c38d30ab6352b3a2883fd06ba17
Sandra <sandrahagevall@hotmail.com>
2026-01-20 16:10:37 +0100

    Add basic Happy Thoughts API routes with documentation endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import cors from "cors"
 import express from "express"
+import listEndpoints from "express-list-endpoints"
+import thoughtData from "./data.json" with { type: "json" }
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -13,7 +15,27 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  const endpoints = listEndpoints(app)
+
+  res.json({
+    message: "Welcome to Happy Thoughts API",
+    endpoints: endpoints
+  })
+})
+
+app.get("/thoughts", (req, res) => {
+  res.json(thoughtData)
+})
+
+app.get("/thoughts/:id", (req, res) => {
+  const id = req.params.id
+  const thought = thoughtData.find((thought) => thought._id === id)
+
+  if (!thought) {
+    return res.status(404).json({ error: `thought with id ${id} does not exist` })
+  }
+
+  res.json(thought)
 })
 
 // Start the server
