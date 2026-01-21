652b218005725fd838e2f9235512a6143c6e191c
Sandra <sandrahagevall@hotmail.com>
2026-01-21 15:12:39 +0100

    Add filtering by hearts and default date sorting (newest thought first) to thoughts endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import cors from "cors"
 import express from "express"
 import listEndpoints from "express-list-endpoints"
-import thoughtData from "./data.json" with { type: "json" }
+import thoughtData from "./data.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -23,10 +23,24 @@ app.get("/", (req, res) => {
   })
 })
 
+// Endpoint to get all thoughts, sorted by date (most recent first)
+// Query params 'hearts' can be used to filter thoughts with minimum number of hearts
 app.get("/thoughts", (req, res) => {
-  res.json(thoughtData)
+  const { hearts } = req.query
+
+  let filteredThoughts = thoughtData
+
+  if (hearts) {
+    const minHearts = Number(hearts)
+    filteredThoughts = filteredThoughts.filter((thought) => thought.hearts >= minHearts)
+  }
+
+  const sortThoughtByDate = [...filteredThoughts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
+  res.json(sortThoughtByDate)
 })
 
+// Endpoint to get a single thought by its id
 app.get("/thoughts/:id", (req, res) => {
   const id = req.params.id
   const thought = thoughtData.find((thought) => thought._id === id)
